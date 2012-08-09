1f3b34d45d4f5e26a217f4c95057a1435e4adb43
amit <amit@Nebula>
2012-08-09 19:16:06 +0530

    Initial Commit.

---
+++ SpringBatch/src/test/java/com/app/learning/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package com.app.learning;
+
+import com.app.learning.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
